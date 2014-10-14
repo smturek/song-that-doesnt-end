1a390fe9004d3eee9d24944f520de94bc33874ce
smturek <smturek@gmail.com>
2014-10-14 10:43:55 -0600

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
   root to: "pages#index"
+
+  get "/verse2" => "pages#verse2", as: :verse2
+
+  get "/verse3" => "pages#verse3", as: :verse3
+
+  get "/verse4" => "pages#verse4", as: :verse4
+
 end
