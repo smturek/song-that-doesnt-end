b1436f1a1fa999100067100afad6dc4c3b144d2b
smturek <smturek@gmail.com>
2014-10-14 10:53:06 -0600

    Commented out test code

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
 
   root to: "pages#index"
 
-  get "/verse2" => "pages#verse2", as: :verse2
+  get "/verse-2" => "pages#verse2", as: :verse2
 
-  get "/verse3" => "pages#verse3", as: :verse3
+  get "/verse-3" => "pages#verse3", as: :verse3
 
-  get "/verse4" => "pages#verse4", as: :verse4
+  get "/verse-4" => "pages#verse4", as: :verse4
 
 end
